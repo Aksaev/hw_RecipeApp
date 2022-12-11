3153564dbcd1536ae91d906832e44d23b5f06182
Aksaev <mail-ax@ya.ru>
2022-12-11 20:58:55 +0400

    Initial commit

---
+++ src/test/java/com/example/hw_recipeapp/HwRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw_recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
